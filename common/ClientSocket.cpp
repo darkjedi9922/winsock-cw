bb638d97ebce64a2468ae3a112c57a66a5bdd9a6
Jed Dark <darkjedi9922@gmail.com>
2019-11-07 17:15:02 +0200

    Add forgotten files.

---
+++ common/ClientSocket.cpp
@@ -0,0 +1,65 @@
+#include "ClientSocket.h"
+#include <WS2tcpip.h>
+
+using namespace std;
+
+ClientSocket::ClientSocket(WinSock *) :
+    QObject(),
+    socket(INVALID_SOCKET)
+{}
+ClientSocket::~ClientSocket() {}
+
+void ClientSocket::connect(string ip, string port)
+{
+    if (socket != INVALID_SOCKET) throw QString("This socket was connected already.");
+
+    struct addrinfo *result = nullptr, *ptr = nullptr, hints;
+    SOCKET connectSocket = INVALID_SOCKET;
+    int iResult;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(ip.c_str(), port.c_str(), &hints, &result);
+    if (iResult != 0) {
+        throw QString("getaddrinfo failed with error: %1").arg(iResult);
+    }
+
+    // Attempt to connect to an address until one succeeds
+    for (ptr = result; ptr != nullptr; ptr = ptr->ai_next) {
+
+        // Create a SOCKET for connecting to server
+        connectSocket = ::socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (connectSocket == INVALID_SOCKET) {
+            throw QString("socket failed with error: %1").arg(WSAGetLastError());
+        }
+
+        // Connect to server.
+        iResult = ::connect(connectSocket, ptr->ai_addr,
+                            static_cast<int>(ptr->ai_addrlen));
+
+        if (iResult == SOCKET_ERROR) {
+            closesocket(connectSocket);
+            connectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (connectSocket == INVALID_SOCKET) {
+        throw QString("Unable to connect to server!");
+    }
+}
+
+void ClientSocket::close()
+{
+    if (socket == INVALID_SOCKET) return;
+    shutdown(socket, SD_BOTH);
+    closesocket(socket);
+    socket = INVALID_SOCKET;
+}
---
+++ common/ClientSocket.h
@@ -0,0 +1,27 @@
+#ifndef CLIENTSOCKET_H
+#define CLIENTSOCKET_H
+
+#include <WinSock2.h>
+#include "WinSock.h"
+#include <QObject>
+#include <string>
+
+class ClientSocket : public QObject
+{
+    Q_OBJECT
+
+public:
+    ClientSocket(WinSock *);
+    ~ClientSocket();
+
+    void connect(std::string ip, std::string port);
+    void close();
+
+signals:
+    void errorRaised(const QString &msg);
+
+private:
+    SOCKET socket;
+};
+
+#endif // CLIENTSOCKET_H
