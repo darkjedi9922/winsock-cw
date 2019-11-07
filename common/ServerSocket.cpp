c7a110d0891057a5df30fe0e04e353b27a41dc9c
Jed Dark <darkjedi9922@gmail.com>
2019-11-07 08:59:52 +0200

    Implement wsa event select mechanism.

--- common/ServerSocket.cpp
+++ common/ServerSocket.cpp
@@ -5,24 +5,68 @@
 using namespace std;
 
 ServerSocket::ServerSocket(WinSock *) noexcept :
-    listenSocket(INVALID_SOCKET)
+    QObject(),
+    listenSocket(INVALID_SOCKET),
+    subscribersCount(0),
+    timerId(-1)
 {}
 
 void ServerSocket::listen(string port)
 {
     if (listenSocket != INVALID_SOCKET) throw QString("Socket is listening already");
-
     listenSocket = bindSocket(port);
     int result = ::listen(listenSocket, SOMAXCONN);
+
     if (result == SOCKET_ERROR) {
+        closesocket(listenSocket);
+        listenSocket = INVALID_SOCKET;
         throw QString("listen failed with error: %1").arg(WSAGetLastError());
     }
+
+    subscribe(listenSocket, FD_ACCEPT);
+    timerId = startTimer(100);
 }
 
 void ServerSocket::close() noexcept
 {
-    if (listenSocket != INVALID_SOCKET) closesocket(listenSocket);
+    if (listenSocket == INVALID_SOCKET) return;
+    killTimer(timerId);
+    removeSubscribe(listenSocket);
+    closesocket(listenSocket);
     listenSocket = INVALID_SOCKET;
+}
+
+void ServerSocket::timerEvent(QTimerEvent *)
+{
+    WSANETWORKEVENTS networkEvents;
+    DWORD index = WSAWaitForMultipleEvents(subscribersCount, subscribedEvents,
+                                           false, 100, false);
+    WSAEnumNetworkEvents(subscribedSockets[index - WSA_WAIT_EVENT_0],
+            subscribedEvents[index - WSA_WAIT_EVENT_0],
+            &networkEvents);
+
+    if (networkEvents.lNetworkEvents & FD_ACCEPT) {
+        if (networkEvents.iErrorCode[FD_ACCEPT_BIT] != 0) {
+            emit errorRaised(
+                        QString("FD_ACCEPT failed with error %1")
+                        .arg(networkEvents.iErrorCode[FD_ACCEPT_BIT])
+            );
+            return;
+        }
+    }
+
+    if (networkEvents.lNetworkEvents & FD_CLOSE) {
+        if (networkEvents.iErrorCode[FD_CLOSE_BIT] != 0) {
+            emit errorRaised(
+                        QString("FD_CLOSE failed with error %1")
+                        .arg(networkEvents.iErrorCode[FD_CLOSE_BIT])
+            );
+            return;
+        }
+
+        removeSubscribe(subscribedSockets[index - WSA_WAIT_EVENT_0]);
+        closesocket(subscribedSockets[index - WSA_WAIT_EVENT_0]);
+    }
 }
 
 SOCKET ServerSocket::bindSocket(string port)
@@ -54,7 +98,7 @@ SOCKET ServerSocket::bindSocket(string port)
         throw QString("socket failed with error: %1").arg(WSAGetLastError());
     }
 
-    iResult = bind(resultSocket,
+    iResult = ::bind(resultSocket,
                    addressInfo->ai_addr,
                    static_cast<int>(addressInfo->ai_addrlen));
 
@@ -66,4 +110,30 @@ SOCKET ServerSocket::bindSocket(string port)
 
     freeaddrinfo(addressInfo);
     return resultSocket;
+}
+
+void ServerSocket::subscribe(SOCKET socket, long events)
+{
+    WSAEVENT newEvent = WSACreateEvent();
+    WSAEventSelect(socket, newEvent, events);
+    subscribedSockets[subscribersCount] = socket;
+    subscribedEvents[subscribersCount] = newEvent;
+    subscribersCount += 1;
+}
+
+void ServerSocket::removeSubscribe(SOCKET socket) noexcept
+{
+    size_t index;
+    for (index = 0; index < subscribersCount; ++index) {
+        if (subscribedSockets[index] == socket) break;
+    }
+
+    if (index == subscribersCount) return;
+
+    for (auto i = index; i < subscribersCount - 1; ++i) {
+        subscribedSockets[i] = subscribedSockets[i + 1];
+        subscribedEvents[i] = subscribedEvents[i + 1];
+    }
+
+    subscribersCount -= 1;
 }
--- common/ServerSocket.h
+++ common/ServerSocket.h
@@ -1,22 +1,37 @@
 #ifndef SERVERSOCKET_H
 #define SERVERSOCKET_H
 
+#include <QObject>
 #include "WinSock.h"
 #include <WinSock2.h>
 #include <string>
 
-class ServerSocket
+class ServerSocket : public QObject
 {
+    Q_OBJECT
+
 public:
     ServerSocket(WinSock *) noexcept;
 
     void listen(std::string port);
     void close() noexcept;
 
+signals:
+    void errorRaised(const QString &message);
+
+protected:
+    virtual void timerEvent(QTimerEvent *);
+
 private:
     SOCKET listenSocket;
+    SOCKET subscribedSockets[WSA_MAXIMUM_WAIT_EVENTS];
+    WSAEVENT subscribedEvents[WSA_MAXIMUM_WAIT_EVENTS];
+    size_t subscribersCount;
+    int timerId;
 
     SOCKET bindSocket(std::string port);
+    void subscribe(SOCKET socket, long events);
+    void removeSubscribe(SOCKET socket) noexcept;
 };
 
 #endif // SERVERSOCKET_H
